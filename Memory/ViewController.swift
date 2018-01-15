9dae7bf65fa2669ce5d2eb9dca8ee95de193d33a
Patrick Kalkman <patrick@simpletechture.nl>
2018-01-15 20:01:59 +0100

    Initial Commit

---
+++ Memory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memory
+//
+//  Created by Patrick Kalkman on 15/01/2018.
+//  Copyright © 2018 SimpleTechture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
